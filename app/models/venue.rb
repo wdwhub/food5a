24fc0fcad5019ef0397df0fb4dd1858527a5c59e
Carl Tanner <rocketeer.captproton@gmail.com>
2016-08-08 11:14:24 -0700

    ADDED: swapped out dummy info for actual venue info

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,6 @@
 class Venue < ApplicationRecord
-  has_many :dfb_articles
-  has_many :dfb_eatery, :through => :dfb_articles
+  has_many  :dfb_articles
+  has_many  :dfb_eatery, :through => :dfb_articles
+  has_one   :foursquare_eatery
+  has_many  :cached_photos
 end
