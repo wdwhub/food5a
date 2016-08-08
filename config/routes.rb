24b0d104d83773618c581d18cf53ba25e1a4f4c7
Carl Tanner <rocketeer.captproton@gmail.com>
2016-08-07 20:53:32 -0700

    INIT: venue show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/backstage', as: 'rails_admin'
   resources :districts
   
-  root to: 'districts#index'
+  # root to: 'districts#index'
   
 end
