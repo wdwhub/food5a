3a75790ce323d1fa8af66b6e400b63964f2c2175
Carl Tanner <rocketeer.captproton@gmail.com>
2016-11-21 19:32:16 -0800

    FIXED: bad migration

--- db/migrate/20161119024616_fix_fousquare_column.rb
+++ db/migrate/20161119024616_fix_fousquare_column.rb
@@ -1,6 +1,5 @@
 class FixFousquareColumn < ActiveRecord::Migration[5.0]
   def change
     rename_column :cached_photos, :fousquare_user, :foursquare_user_name
-    rename_column :photos, :fousquare_user, :foursquare_user_name
   end
 end
